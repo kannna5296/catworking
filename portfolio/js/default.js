d8aea729c097335ca8c9045029646a5da0264365
kannna5296 <kannnahacks5296@gmail.com>
2020-11-22 21:44:46 +0900

    ゆっくりスクロール

---
+++ portfolio/js/default.js
@@ -0,0 +1,8 @@
+//ページ内リンクスムーススクロール
+$('a[href^="#"]').on('click', function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop: position}, 550, "swing");
+    return false;
+  });
