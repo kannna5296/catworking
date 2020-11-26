285c062d7ec927e33e85cf5659f53b1e8fce6e74
kannna5296 <kannnahacks5296@gmail.com>
2020-11-26 20:25:11 +0900

    レスポンシブ用ヘッダ

--- portfolio/js/jquery.js
+++ portfolio/js/jquery.js
@@ -17,3 +17,17 @@ $(function(){
         }, 200);
     });
 });
+
+//上スクロールで出てくる、下スクロールで消える
+var startPos = 0,winScrollTop = 0;
+$(window).on('scroll',function(){
+    winScrollTop = $(this).scrollTop();
+    if (winScrollTop >= startPos) {
+        if(winScrollTop >= 200){
+        $('.site-header').addClass('hide');
+        }
+    } else {
+        $('.site-header').removeClass('hide');
+    }
+    startPos = winScrollTop;
+});
