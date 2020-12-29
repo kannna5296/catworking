923cb446a6989e9d5be3d1736ccea43d2ba8d8bf
kannna5296 <kannnahacks5296@gmail.com>
2020-12-29 21:53:21 +0900

    ハンバーガーメニュー

--- 04_middleclass/js/jquery.js
+++ 04_middleclass/js/jquery.js
@@ -1,17 +1,17 @@
-$(function(){
-    $(".btn-gnavi").on("click", function(){
+$(function () {
+    $(".btn-gnavi").on("click", function () {
         // ハンバーガーメニューの位置を設定
         var rightVal = 0;
-        if($(this).hasClass("open")) {
+        if ($(this).hasClass("open")) {
             // 位置を移動させメニューを開いた状態にする
-            rightVal = -300;
+            rightVal = -window.outerWidth;
             // メニューを開いたら次回クリック時は閉じた状態になるよう設定
             $(this).removeClass("open");
         } else {
             // メニューを開いたら次回クリック時は閉じた状態になるよう設定
             $(this).addClass("open");
         }
- 
+
         $("#globalnavi").stop().animate({
             right: rightVal
         }, 200);
