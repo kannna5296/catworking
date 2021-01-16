37d833b3561bef3e4aa6c519c18e220fea96e5a3
kannna5296 <kannnahacks5296@gmail.com>
2021-01-16 12:37:55 +0900

    ハンバーガーメニュー修正

--- 05_middleclassEX/js/script.js
+++ 05_middleclassEX/js/script.js
@@ -3,16 +3,25 @@ $(function () {
         $('body').toggleClass('is-drawerActive')
 
         if ($(this).attr('aria-expanded') == 'false') {
-            $(this).attr('aria-expanded', 'true')
+            $(this).attr('aria-expanded', true)
+            $('#js-global-menu').css('visibility', 'visible')
             $('#js-global-menu').attr('area-hidden', 'false')
         } else {
-            $(this).attr('aria-expanded', 'false')
+            $(this).attr('aria-expanded', false)
+            $('#js-global-menu').css('visibility', 'hidden')
             $('#js-global-menu').attr('area-hidden', 'true')
         }
     })
+    //背景の黒ボックスクリックでもドロワーが消えるようにする
     $('#js-drawer-background').click(function () {
         $('body').removeClass('is-drawerActive')
-        $('#js-hamburger').attr('aria-expanded', 'false')
+
+        //are-expandedはtrueの想定なのでif文なし
+        //ハンバーガーメニュー元に戻す
+        $('#js-hamburger').attr('aria-expanded', false)
+        //ドロワー消す
+        $('#js-global-menu').css('visibility', 'hidden')
         $('#js-global-menu').attr('area-hidden', 'true')
+
     })
 });
